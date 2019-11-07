f886fb8fc889b555cffde1a38c4b3f0b777c7505
Operel <oriperelman@gmail.com>
2019-11-07 15:54:22 +0200

    Paulina Test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularTest
+# PaulinaTest
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.17.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlowManagementComponent } from './flow-management/flow-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowManagementComponent
   ],
   imports: [
     BrowserModule,
