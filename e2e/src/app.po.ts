dfe5ebecd4ec967a424a68f7adaf33bd005f890e
Operel <oriperelman@gmail.com>
2019-11-14 11:39:51 +0200

    Add one e2e test using angular cli built in protractor. Test navigates to app's root and expects Okta login.

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,24 @@
 import { browser, by, element } from 'protractor';
 
+import { env } from '../../src/environments/environment';
+
+const rootUrl = `http://localhost:${env.PORT}`;
+
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateToAppRoot() {
+    browser.get(rootUrl);
+    browser.sleep(1000);
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getOktaLoginElementsText() {
+    return element(by.css('.okta-form-title.o-form-head')).getText();
   }
+
+  // navigateTo() {
+  //   return browser.get(browser.baseUrl) as Promise<any>;
+  // }
+
+  // getTitleText() {
+  //   return element(by.css('app-root .content span')).getText() as Promise<string>;
+  // }
 }
